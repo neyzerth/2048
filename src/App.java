ee62d6f0d5430323acbbaab58c47c952a9e36aa5
Neyzer <neyzertoledo@gmail.com>
2024-08-19 17:31:13 -0700

    can move in the right direction

--- src/App.java
+++ src/App.java
@@ -1,6 +1,13 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        Table test = new Table();
-        test.print();
+        Table test = new Table(4,8,1);
+        
+        
+        while(true){
+            Text.clear();
+            test.print();
+            test.moveRight();
+            Text.waitEnter();
+        }
     }
 }
--- src/Table.java
+++ src/Table.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Random;
 
 public class Table {
@@ -9,6 +10,21 @@ public class Table {
         randomSquare();
     }
 
+    public Table(int... pos){
+        int x,y;
+
+        for (int i : pos) {
+            x = i <= 4 ? 0 : 
+                i <= 8 ? 1 : 
+                i <= 12 ? 2 : 
+                i <= 16 ? 3 : -1;
+                
+            y = i - (x * 4) - 1;
+
+
+            rows[x][y] = new Square();
+        }
+    }
     public void randomSquare(){
         Random rd = new Random();
         boolean empty;
@@ -53,6 +69,104 @@ public class Table {
         }
 
     }
+
+    public void moveRight(){
+        Square [][] temp = new Square[4][4];
+        for (int i = 0; i < 4; i++) 
+            for (int j = 0; j < 4; j++)
+                temp[i][j] = this.rows[i][j]; 
+
+        for (int i = 0; i < 4; i++) {
+            Square [] row = rows[i];
+
+            for (int j = 3; j >= 0; j--) {
+                //guarda el cuadro iterado
+                Square sq = row[j];
+
+                //si es null, no hace nada
+                if(sq == null) continue;
+
+    
+                int k = j; //contador para recorrer los cuadros a su derecha
+                //recorre hasta llegar a un cuadro no vacio
+                while((k) < 3 && row[k+1] == null)
+                    k++;
+
+                
+                //deja vacio la posicion anterior
+                row[j] = null;
+
+                //si no esta en la ultima posicion, intenta combinarse con el siguiente
+            
+                if(!(k < 3) || !sq.merge(row[k+1])){
+                    row[k] = sq;
+                    continue;
+                }
+                row[k] = null;
+                row[k+1] = sq;
+            
+                                
+            }
+
+        }
+        if(!compareTable(temp)) 
+            randomSquare();
+    }
+
+    private boolean compareTable(Square[][] table){
+        for (int i = 0; i < 4; i++) 
+            for (int j = 0; j < 4; j++) 
+                if (table[i][j] != this.rows[i][j])
+                    return false;
+            
+        
+        return true;
+    }
+
+    public void moveLeft(){
+        Square [][] temp = new Square[4][4];
+        for (int i = 0; i < 4; i++) 
+            for (int j = 0; j < 4; j++)
+                temp[i][j] = this.rows[i][j]; 
+
+        for (int i = 0; i < 4; i++) {
+            Square [] newRow = new Square[4];
+
+            newRow = rows[i];
+            for (int j = 0; j < 4; j++) {
+                //guarda el cuadro iterado
+                Square sq = rows[i][j];
+
+                //si es null, no hace nada
+                if(sq == null) continue;
+
+    
+                int k = j; //contador para recorrer los cuadros a su derecha
+                //recorre hasta llegar a un cuadro no vacio
+                while((k) > 0 && rows[i][k-1] == null)
+                    k--;
+
+                //deja vacio la posicion anterior
+                newRow[j] = null;
+
+                //si no esta en la ultima posicion, intenta combinarse con el siguiente
+                if((k) > 0 && sq.merge(newRow[k-1])){
+                    newRow[k] = null;
+                    newRow[k-1] = sq;
+                } else {
+                    newRow[k] = sq;
+                }
+                                
+                rows[i] = newRow;
+            }
+
+        }
+        if(!Arrays.equals(this.rows, temp)) 
+            randomSquare();
+
+    }
+
+    
 
     private String numFormat(int number){
         int numLong = String.valueOf(number).length();
